61e2428f37e44528a437226b1e4a0b287cb96c4f
LChaveneau <chaveneau.lucas@gmail.com>
2022-03-11 17:50:11 +0100

    Genre a terminer

--- Lucas/app/app.R
+++ Lucas/app/app.R
@@ -28,6 +28,22 @@ df2 <- df2  %>%
   mutate(Annee = as.factor(Annee)) %>% 
   mutate(platform = as.factor(platform))
 
+switch_vector <- function(x){switch(
+  x,
+  "Action" = df2$Action,
+  "Aventure" = df2$Aventure,
+  "Puzzle" = df2$Puzzle,
+  "Racing" = df2$Racing,
+  "Education" = df2$Educational,
+  "Compilation" = df2$Compilation,
+  "Simulation" = df2$Simulation,
+  "Sports" = df2$Sports,
+  "Strategie" = df2$Strategie,
+  "Role_play" = df2$Role_play,
+  "Edition_special" = df2$Edition_special,
+  "DLC" = df2$DLC)
+}
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(  
 
@@ -100,12 +116,38 @@ ui <- fluidPage(
                               max = 2017,
                               value = c(1980,2017)),
                   
-                  selectInput("plateform1", label = h3("Plateform"), 
-                              choices = c("--", as.list(levels(df2$Platform))),
-                              selected = "--"),
-                  selectInput("genre1", label = h3("Type"), 
-                              choices = c("--", as.list(levels(df2$Genre))),
-                              selected = "--")
+                  selectizeInput("plateform1", 
+                                 label = h3("Plateform"),
+                                 choices = c(levels(df2$Platform)),
+                                 multiple = TRUE,
+                                 options = list(dropdownParent = 'body', 
+                                                maxitems = 'null', 
+                                                plugins = list("remove_button"),
+                                                highlight = F,
+                                                closeAfterSelect = T)
+                  ),
+                  selectizeInput("genre1",
+                                 label = h3('Genre'),
+                                 choices = c(
+                                   "Action",
+                                   "Aventure",
+                                   "Puzzle",
+                                   "Racing",
+                                   "Educational",
+                                   "Compilation",
+                                   "Simulation",
+                                   "Sports",
+                                   "Strategie",
+                                   "Role_play",
+                                   "Edition_special",
+                                   "DLC"),
+                                 multiple = TRUE,
+                                 options = list(dropdownParent = 'body', 
+                                                maxitems = 'null', 
+                                                plugins = list("remove_button"),
+                                                highlight = F,
+                                                closeAfterSelect = T)
+                  )
                 ),
                 mainPanel(
                   DTOutput("Tablo")
@@ -118,18 +160,18 @@ ui <- fluidPage(
                selectizeInput("Test",
                               label = h3('Test'),
                               choices = c(
-                                          "Action",
-                                          "Aventure",
-                                          "Puzzle",
-                                          "Racing",
-                                          "Educational",
-                                          "Compilation",
-                                          "Simulation",
-                                          "Sports",
-                                          "Strategie",
-                                          "Role_play",
-                                          "Edition_special",
-                                          "DLC"),
+                                          "Action" = "Action",
+                                          "Aventure" = "Aventure",
+                                          "Puzzle" = "Puzzle",
+                                          "Course" = "Racing",
+                                          "Education" = "Educational",
+                                          "Compilation" = "Compilation",
+                                          "Simulation" = "Simulation",
+                                          "Sports" = "Sports",
+                                          "Strategie" = "Strategie",
+                                          "Role play" = "Role_play",
+                                          "Edition special" = "Edition_special",
+                                          "DLC" = "DLC"),
                               multiple = TRUE,
                               options = list(dropdownParent = 'body', 
                                              maxitems = 'null', 
@@ -146,24 +188,44 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+  #############################################################################################
+  ###################################### OBSERVE ##############################################
+  #############################################################################################
+  
   observe({
     input$plateform1
-    
-    if(input$plateform1 != '--'){
-    minimum <- min(na.omit(as.numeric(as.character(df$Year[df$Platform==input$plateform1]))))
-    maximum <- max(na.omit(as.numeric(as.character(df$Year[df$Platform==input$plateform1]))))
-    }else{
-      minimum <- 1980
-      maximum <- 2017
+
+    if(input$type_recommandation == "Vente"){
+      if(is.null(input$plateform1) == FALSE){
+      minimum <- min(na.omit(as.numeric(as.character(df$Year[df$Platform==input$plateform1]))))
+      maximum <- max(na.omit(as.numeric(as.character(df$Year[df$Platform==input$plateform1]))))
+      }else{
+        minimum <- 1980
+        maximum <- 2017
+      }
+      updateSliderInput(
+        inputId = "years3",
+        min = minimum,
+        max = maximum,
+        value = c(minimum,maximum)
+        )
     }
-    updateSliderInput(
-      inputId = "years3",
-      min = minimum,
-      max = maximum,
-      value = c(minimum,maximum)
+    if(input$type_recommandation == "Popularite"){
+      if(is.null(input$plateform1) == FALSE){
+        minimum <- min(na.omit(as.numeric(as.character(df2$Annee[df2$platform==input$plateform1]))))
+        maximum <- max(na.omit(as.numeric(as.character(df2$Annee[df2$platform==input$plateform1]))))
+      }else{
+        minimum <- 1950
+        maximum <- 2022
+      }
+      updateSliderInput(
+        inputId = "years3",
+        min = minimum,
+        max = maximum,
+        value = c(minimum,maximum)
       )
-    
-  })
+    }
+    })
   
   observe({
     input$type_recommandation
@@ -176,16 +238,13 @@ server <- function(input, output) {
         max = 2017,
         value = c(1980,2017)
         )
-
-      updateSelectInput(
+      
+      updateSelectizeInput(
         inputId = "plateform1",
-        choices = c("--", as.list(levels(df$Platform))),
-        selected = "--"
-      )
+        choices = c(levels(df$Platform)))
 
-      updateSelectInput(inputId = "genre1",
-                        choices = c("--", as.list(levels(df$Genre))),
-                        selected = "--")
+      updateSelectizeInput(inputId = "genre1",
+                        choices = c(levels(df$Genre)))
     }
     if(input$type_recommandation == "Popularite"){
       updateSliderInput(
@@ -195,29 +254,32 @@ server <- function(input, output) {
         value = c(1950,2022)
       )
 
-      updateSelectInput(
+      updateSelectizeInput(
         inputId = "plateform1",
-        choices = c("--", as.list(levels(df2$platform))),
-        selected = "--"
+        choices = c(levels(df2$platform))
       )
-      updateSelectInput(inputId = "genre1",
-                        choices = c("--",
-                                    "Action",
-                                    "Aventure",
-                                    "Puzzle",
-                                    "Racing",
-                                    "Educational",
-                                    "Compilation",
-                                    "Simulation",
-                                    "Sports",
-                                    "Strategie",
-                                    "Role_play",
-                                    "Edition_special",
-                                    "DLC"),
-                        selected = "--")
+      
+      updateSelectizeInput(inputId = "genre1",
+                        choices = c(
+                          "Action" = "Action",
+                          "Aventure" = "Aventure",
+                          "Puzzle" = "Puzzle",
+                          "Course" = "Racing",
+                          "Education" = "Educational",
+                          "Compilation" = "Compilation",
+                          "Simulation" = "Simulation",
+                          "Sports" = "Sports",
+                          "Strategie" = "Strategie",
+                          "Role play" = "Role_play",
+                          "Edition special" = "Edition_special",
+                          "DLC" = "DLC"))
     }
     })
-    
+  
+  ####################################################################################
+  ####################### Reactive SWITCH ###########################################
+  ####################################################################################
+  
   variable <- reactive({switch(input$var,
                                "Publisher" = df$Publisher, 
                                "Genre" = df$Genre)
@@ -230,7 +292,15 @@ server <- function(input, output) {
                                "Name" = df$Name)
   })
   
-    output$Hist <- renderPlot({
+  variable_genre <- reactive({
+    sapply(input$genre1, switch_vector)
+  }) 
+  
+  ###################################################################################
+  ############################## OUTPUT #############################################
+  ####################################################################################
+  
+  output$Hist <- renderPlot({
           fct_count(variable()[df$Year == input$years], sort = T) %>%
           slice(1:10) %>%
           ggplot() +
@@ -239,7 +309,7 @@ server <- function(input, output) {
           coord_flip()
       })
 
-    output$SalesHist <- renderPlot({
+  output$SalesHist <- renderPlot({
       df %>% 
         filter(Year == input$years2) %>% 
         select(everything()) %>% 
@@ -253,28 +323,29 @@ server <- function(input, output) {
         geom_col() + 
         coord_flip()
     })
-    output$Tablo <- renderDT({
-      ####### PAR SALES ########3
+  output$Tablo <- renderDT({
+    
+      ####### PAR SALES ########
         if(input$type_recommandation == "Vente"){
 
         image <- paste0("<img src=\"", src="https://www.mobygames.com/images/covers/s/264995-air-hockey-android-front-cover.jpg", "\" height=\"30\" data-toggle=\"tooltip\" data-placement=\"right\" title=\"", "bijo", "\"></img>")
         
-        tablo <-df %>%
+        tablo <- df %>%
           filter(as.numeric(as.character(Year)) >= input$years3[1]) %>%
           filter(as.numeric(as.character(Year)) <= input$years3[2])
         
-        if(input$plateform1 != "--"){
+        if(is.null(input$plateform1) == FALSE){
           tablo <- tablo %>% 
             filter(Platform == input$plateform1)
           }
-        if(input$genre1 != "--"){
+        if(is.null(input$genre1) == FALSE){
           tablo <- tablo %>% 
             filter(Genre == input$genre1)
         }
         best_choice_tableau <- tablo %>% 
           slice(1:3) %>% 
           select("Name":"Publisher") %>% 
-          mutate(picture = c(image, image, image)) %>% 
+          #mutate(picture = c(image, image, image)) %>% 
           datatable(rownames = F,
                     extensions = c('Select'),
                     selection = "single",
@@ -284,10 +355,28 @@ server <- function(input, output) {
                       ordering = F),
                     escape = F) 
         }
+    
       ###############PAR POPULARITE############
     if(input$type_recommandation == "Popularite"){
-      best_choice_tableau <- df2[,1:3] %>% slice(1:3) %>%
-        select(everything()) %>% 
+      
+      tablo <-df2 %>%
+        filter(as.numeric(as.character(Annee)) >= input$years3[1]) %>%
+        filter(as.numeric(as.character(Annee)) <= input$years3[2])
+      
+      if(is.null(input$plateform1) == FALSE){
+        tablo <- tablo %>% 
+          filter(platform == input$plateform1)
+      }
+      
+      if(is.null(input$genre1) == FALSE){
+        tablo <- data %>%
+          filter(data %>% select(as.vector(variable_genre())) %>% rowSums() > 0)
+      }
+      
+      best_choice_tableau <- tablo %>% 
+        arrange(reviews) %>% 
+        slice(1:3) %>%
+        select(Name, Publisher, Annee, platform) %>% 
         datatable(rownames = F,
                   extensions = c('Select'),
                   selection = "single",
@@ -309,18 +398,3 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
-# output$SalesHist <- renderPlot({
-#   tablo <- df %>% 
-#     filter(Year == input$years2) %>% 
-#     select(everything()) %>% 
-#     group_by(variable2()[df$Year == input$years2]) %>%
-#     summarize(Total_sales = sum(Global_Sales))%>%
-#     arrange(desc(Total_sales)) %>%
-#     slice(1:10)
-#   colnames(tablo) <- c("Truc", "Total_sales")
-#   ggplot(tablo) + 
-#     aes(x=fct_reorder(Truc, Total_sales), y = Total_sales) + 
-#     geom_col() + 
-#     coord_flip()
-# })
